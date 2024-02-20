2a91e8a9d95ca89eed05866bb8ab384b85185d88
unknown <polar940301@gmail.com>
2024-02-20 22:04:20 +0800

    gg

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,8 @@
 import config from '../config.json';
-const closeWhenEnd = false; //whether to close the browser after shxt.
-const showViewPort = true; //whether to show the browser that you can see how it's going
 
 require(`./shxt`)({
     id: config.student_id,
     pwd: config.password,
     manual: config.manual,
-    showViewPort: showViewPort,
+    showViewPort: config.showViewPort,
 });
-
