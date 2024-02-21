56d66b35378b00fba5e673ec43eac060b2b3719a
unknown <polar940301@gmail.com>
2024-02-21 11:25:41 +0800

    courses signing & loading check speed improvement

--- src/shxt.ts
+++ src/shxt.ts
@@ -23,18 +23,23 @@ module.exports = async ({ id = '', pwd = '', showViewPort = true, manual = false
     await (await page.waitForXPath(`//*[@id="ContentPlaceHolder1_BtnLoginNew"]`) as ElementHandle<Element>).click();
     const switchBTN = await page.waitForXPath(`//*[@id="ContentPlaceHolder1_Button7"]`);
 
-    if (!(switchBTN instanceof ElementHandle)) {
-        console.log(chalk.red('The PASSWORD or ID you provided is wrong.'));
-        process.exit(1);
-    } else {
+    if (!switchBTN)
+        console.log(chalk.red('The PASSWORD or ID you provided was wrong.'));
+    else {
         await (switchBTN as ElementHandle<Element>).click();
-        await delay(500);
         const timeLeft = (new Date(config.time)).getTime() - Date.now();
+        let courses: ElementHandle<HTMLTableRowElement>[] = [], waiting_try_count = 0, course_handled = 0;
+        do {
+            await delay(25);
+            waiting_try_count++;
+            courses = await page.$$('#ContentPlaceHolder1_grd_subjs > tbody > tr');
+        } while (courses.length <= 1 && waiting_try_count < 100);
+        console.log('Found ' + chalk.yellow(courses.length - 1) + ' courses, please check.');
         if (!manual) {
-            console.log(`There\'s ${(timeLeft / 1000 / 60).toFixed(1).toString()} minuts left before the open time. Get ready`);
+            console.log(`${chalk.yellow((timeLeft / 1000 / 60).toFixed(1).toString())} minuts till the open time. Get ready.`);
             setTimeout(async () => {
                 console.log('Found scheduled course(s):');
-                for (const tr of (await page.$$('#ContentPlaceHolder1_grd_subjs > tbody > tr')).slice(1)) {
+                courses.slice(1).forEach(async tr => {
                     let backend_output = '';
                     const add_btn = await tr.$('input');
                     if (((await page.evaluate(add_btn => (add_btn as HTMLInputElement).className, add_btn)).includes('hide')))
@@ -43,12 +48,14 @@ module.exports = async ({ id = '', pwd = '', showViewPort = true, manual = false
                         await (add_btn as ElementHandle<HTMLInputElement>).click();
                         backend_output += chalk.green('new add');
                     }
-                    console.log(backend_output + '\t' + (await page.evaluate(x => x?.innerText, (await tr.$$('td'))[1])) +
-                        '\t' + (await page.evaluate(x => x?.innerText, (await tr.$$('td'))[2])) + '\t');
-                };
+                    console.log(backend_output + '\t' + (await tr.$$eval('td', x => x[1].innerText + '\t' + x[2].innerText)) + '\t');
+                    course_handled++;
+                });
+                while (course_handled != courses.length - 1)
+                    await delay(100);
                 console.log('\n' + chalk.yellow('hint: the ') + chalk.green('new add ') + chalk.yellow('may be a clashed one.' + '\n'
                     + 'To be on the safe side, please CHECK AGAIN all the courses you want are added correctly.'));
             }, timeLeft > 0 ? timeLeft : 0);
         }
     }
-};
+}
